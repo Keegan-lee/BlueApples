9e341b3c35caa3e22c7f139013d13fcd48a14a97
Keegan Francis <keegan.lee.francis@gmail.com>
2017-05-27 23:45:00 -0300

    CI test

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
         'public/bower_components/angular/angular.js',
         'public/bower_components/angular-bootstrap/ui-bootstrap.js',
         'public/bower_components/angular-ui-router/release/angular-ui-router.js',
-        'node_modules/angular-mocks/angular-mocks.js',
+        'public/bower_components/angular-mocks/angular-mocks.js',
         'public/js/**/*.js',
         'src/tests/*.js'
     ],
